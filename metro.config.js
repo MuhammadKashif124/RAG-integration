9e07c2e03bd37cb6b7a512f6cf60e41e3567a93e
Kashif Tariq <kashiftariq7654@gmail.com>
2025-09-24 10:38:36 -0700

    fix icon issue

--- metro.config.js
+++ metro.config.js
@@ -8,9 +8,9 @@ config.server = {
   enhanceMiddleware: (middleware) => {
     return (req, res, next) => {
       // Add CORS headers
-      res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+      res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
       
       // Handle preflight requests
       if (req.method === 'OPTIONS') {
